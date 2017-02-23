3c00b91b7d63937233b54423cea85714ebcd1bbc
Justin Handville <nanolith@gmail.com>
2017-02-23 16:22:09 -0500

    Add support for model check includes.

--- model_check.mk
+++ model_check.mk
@@ -1,5 +1,7 @@
 #override this variable with the location of this model checking library
 MODEL_CHECK_DIR?=$(PWD)
-CBMC_OPTS=-I $(MODEL_CHECK_DIR)/include -DCBMC --trace --beautify \
-          --bounds-check --pointer-check --div-by-zero-check --memory-leak-check
+MODEL_CHECK_INCLUDES=-I $(MODEL_CHECK_DIR)/include
+
+CBMC_OPTS=$(MODEL_CHECK_INCLUDES) -DCBMC --trace --beautify --bounds-check \
+          --pointer-check --div-by-zero-check --memory-leak-check
 MODEL_CHECK_SOURCES=$(wildcard $(MODEL_CHECK_DIR)/src/*.c)
