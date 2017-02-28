11e2b33d66060facfb6d612f2db4d3df9d257d09
Justin Handville <nanolith@gmail.com>
2017-02-28 18:45:26 -0500

    Added stop-on-fail for easier failure analysis.

--- model_check.mk
+++ model_check.mk
@@ -3,5 +3,6 @@ MODEL_CHECK_DIR?=$(PWD)
 MODEL_CHECK_INCLUDES=-I $(MODEL_CHECK_DIR)/include
 
 CBMC_OPTS=$(MODEL_CHECK_INCLUDES) -DCBMC --trace --beautify --bounds-check \
-          --pointer-check --div-by-zero-check --memory-leak-check
+          --pointer-check --div-by-zero-check --memory-leak-check \
+          --stop-on-fail
 MODEL_CHECK_SOURCES=$(wildcard $(MODEL_CHECK_DIR)/src/*.c)
