154a9d700ae91487931dc7320a1d7e00502704f7
Justin Handville <nanolith@gmail.com>
2017-03-03 18:41:39 -0500

    Set some sane upper-bounds on loop unrolling.

--- model_check.mk
+++ model_check.mk
@@ -2,7 +2,10 @@
 MODEL_CHECK_DIR?=$(PWD)
 MODEL_CHECK_INCLUDES=-I $(MODEL_CHECK_DIR)/include
 
-CBMC_OPTS=$(MODEL_CHECK_INCLUDES) -DCBMC --trace --beautify --bounds-check \
-          --pointer-check --div-by-zero-check --memory-leak-check \
-          --stop-on-fail
+CBMC_STANDARD_OPTS= \
+    $(MODEL_CHECK_INCLUDES) -DCBMC --trace --beautify --bounds-check \
+    --pointer-check --div-by-zero-check --memory-leak-check --stop-on-fail
+CBMC_LOOP_REDUCING_OPTS= \
+    --unwind 60 --unwinding-assertions
+CBMC_OPTS=$(CBMC_STANDARD_OPTS) $(CBMC_LOOP_REDUCING_OPTS)
 MODEL_CHECK_SOURCES=$(wildcard $(MODEL_CHECK_DIR)/src/*.c)
