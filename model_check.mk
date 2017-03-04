3de42e3b63f317d3d3fcb38bb5bf5f5fd8967e93
Justin Handville <nanolith@gmail.com>
2017-03-03 22:43:18 -0500

    Remove beautification option to speed up failures.

--- model_check.mk
+++ model_check.mk
@@ -3,7 +3,7 @@ MODEL_CHECK_DIR?=$(PWD)
 MODEL_CHECK_INCLUDES=-I $(MODEL_CHECK_DIR)/include
 
 CBMC_STANDARD_OPTS= \
-    $(MODEL_CHECK_INCLUDES) -DCBMC --trace --beautify --bounds-check \
+    $(MODEL_CHECK_INCLUDES) -DCBMC --trace --bounds-check \
     --pointer-check --div-by-zero-check --memory-leak-check --stop-on-fail
 CBMC_LOOP_REDUCING_OPTS= \
     --unwind 60 --unwinding-assertions
